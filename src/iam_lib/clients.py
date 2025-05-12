202b2dbaacba958b9d82c5dcc29cd360950a87e5
Mark Servilla <mark.servilla@gmail.com>
2025-05-11 20:30:59 -0600

    Add first IAM client REST API call

---
+++ src/iam_lib/clients.py
@@ -0,0 +1,49 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""
+:Mod:
+    clients
+
+:Synopsis:
+    IAM REST API client interfaces
+
+:Author:
+    servilla
+
+:Created:
+    5/11/25
+"""
+import daiquiri
+
+from iam_lib.response import Response
+from iam_lib.rest_api import Client
+
+
+logger = daiquiri.getLogger(__name__)
+
+
+def add_eml(client: Client, accept: str, token: str, principal: str, eml: str) -> Response:
+    """To parse a valid EML document and add its ACRs to the ACR registry for the resources identified in the EML
+    document
+
+    Args:
+        client (iam_lib.rest_api.Client): IAM REST API client
+        accept (str): Accept format (either JSON or XML)
+        token (str): IAM JWT token
+        principal (str): IAM principal owner (either EDI-ID or IdP identifier)
+        eml (str): EML document
+
+    Returns:
+        iam_lib.response.Response: Response object
+
+    Raises:
+        iam_lib.exceptions.IAMRequestError: On HTTP request error
+        iam_lib.exceptions.IAMResponseError: On non-200 response
+    """
+    route = "/auth/v1/eml"
+    kwargs = {
+        "principal": principal,
+        "eml": eml,
+    }
+    return client.post(token=token, route=route, kwargs=kwargs, accept=accept)
