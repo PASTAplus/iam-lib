fe68a7f22b04cb94337ac2887033ced7710c41e2
Mark Servilla <mark.servilla@gmail.com>
2025-05-16 10:48:28 -0600

    Add shared fixtures module

---
+++ tests/fixtures.py
@@ -0,0 +1,54 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""
+:Mod:
+    fixtures
+
+:Synopsis:
+    Pytest fixtures
+
+:Author:
+    servilla
+
+:Created:
+    5/16/25
+"""
+from datetime import datetime, timedelta, timezone
+
+import pytest
+from requests.cookies import RequestsCookieJar  # For mocking
+from requests.structures import CaseInsensitiveDict  # For mocking
+
+from iam_lib.client import Client
+from utilities import make_token
+
+
+@pytest.fixture(scope="function")
+def client():
+    return Client(
+        scheme="HTTPS",
+        host="localhost",
+        accept="JSON",
+        public_key_path="./data/public_key.pem",
+        algorithm="ES256",
+        token=make_token(datetime.now(tz=timezone.utc) + timedelta(hours=1))
+    )
+
+
+@pytest.fixture(scope="function")
+def cookies():
+    cookies = RequestsCookieJar()
+    cookies.set(
+        name="pasta_token",
+        value=f"{make_token(datetime.now(tz=timezone.utc) + timedelta(hours=1))}",
+        domain="edirepository.org"
+    )
+    return cookies
+
+
+@pytest.fixture(scope="function")
+def headers():
+    headers = CaseInsensitiveDict()
+    headers["Content-Type"] = "application/json"
+    return headers
