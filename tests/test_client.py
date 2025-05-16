9cb7096b3014db9213458be5b86d241814796622
Mark Servilla <mark.servilla@gmail.com>
2025-05-15 21:35:37 -0600

    Update tests with correct mocks

--- tests/test_client.py
+++ tests/test_client.py
@@ -17,23 +17,22 @@ from unittest.mock import MagicMock
 
 import jwt
 import pytest
+import requests
 from requests.cookies import RequestsCookieJar  # For mocking
 from requests.structures import CaseInsensitiveDict  # For mocking
 
 from iam_lib.client import Client
-from iam_lib.response import Response
-
+from utilities import make_token
 
 @pytest.fixture(scope="function")
 def client():
-    token = _make_token(datetime.now(tz=timezone.utc) + timedelta(hours=1))
     return Client(
         scheme="HTTPS",
         host="localhost",
         accept="JSON",
         public_key_path="./data/public_key.pem",
         algorithm="ES256",
-        token=token
+        token=make_token(datetime.now(tz=timezone.utc) + timedelta(hours=1))
     )
 
 
@@ -42,7 +41,7 @@ def cookies():
     cookies = RequestsCookieJar()
     cookies.set(
         name="pasta_token",
-        value=f"{_make_token(datetime.now(tz=timezone.utc) + timedelta(hours=1))}",
+        value=f"{make_token(datetime.now(tz=timezone.utc) + timedelta(hours=1))}",
         domain="edirepository.org"
     )
     return cookies
@@ -55,7 +54,19 @@ def headers():
     return headers
 
 
-def test_api_get(client, cookies, headers, mocker):
+def test_client(client):
+    assert client.scheme == "https"
+    assert client.host == "localhost"
+    assert client.accept == "application/json"
+    assert client.public_key_path == "./data/public_key.pem"
+    assert client.algorithm == "ES256"
+    jwt.decode(
+        client.token,
+        Path(client.public_key_path).read_text().encode("utf-8"),
+        algorithms=client.algorithm
+    )
+
+def test_get(client, cookies, headers, mocker):
     mock_requests_response = MagicMock(
         status_code=200,
         reason="OK",
@@ -63,45 +74,57 @@ def test_api_get(client, cookies, headers, mocker):
         cookies=cookies,
         text='{"GET": "OK"}'
     )
-    mock_client_response = Response(mock_requests_response)
-    mocker.patch.object(client, "get", return_value=mock_client_response)
-    response = client.get(route="/auth/v1/ping")
+    mocker.patch.object(requests, "get", return_value=mock_requests_response)
+    response = client.get(route="auth/v1/ping")
     assert response.status_code == 200
     assert response.body == '{"GET": "OK"}'
 
 
-def test_api_post(client):
+def test_post(client, cookies, headers, mocker):
+    mock_requests_response = MagicMock(
+        status_code=200,
+        reason="OK",
+        headers=headers,
+        cookies=cookies,
+        text='{"POST": "OK"}'
+    )
+    mocker.patch.object(requests, "post", return_value=mock_requests_response)
     parameters = {
         "principal": "EDI-3fa734a7cd6e40998a5c2b5486b6eced",
         "eml": "<eml></eml>"
     }
-    response = client.post(route="/auth/v1/ping", parameters=parameters)
+    response = client.post(route="auth/v1/ping", parameters=parameters)
     assert response.status_code == 200
+    assert response.body == '{"POST": "OK"}'
 
 
-def _make_token(exp: datetime) -> str:
-    payload = {
-        'cn': 'jack',
-        'email': None,
-        'exp': exp,
-        'gn': 'jack',
-        'hd': 'edirepository.org',
-        'iat': 1746738975,
-        'iss': 'https://auth.edirepository.org',
-        'nbf': 1746738975,
-        'pastaGroups': [],
-        'pastaIdentityId': 3,
-        'pastaIdpName': 'ldap',
-        'pastaIdpUid':'uid=jack,o=EDI,dc=edirepository,dc=org',
-        'pastaIsAuthenticated': True,
-        'pastaIsEmailEnabled': False,
-        'pastaIsEmailVerified': False,
-        'pastaIsVetted': True,
-        'sn': None,
-        'sub': 'EDI-3fa734a7cd6e40998a5c2b5486b6eced'
+def test_put(client, cookies, headers, mocker):
+    mock_requests_response = MagicMock(
+        status_code=200,
+        reason="OK",
+        headers=headers,
+        cookies=cookies,
+        text='{"PUT": "OK"}'
+    )
+    mocker.patch.object(requests, "put", return_value=mock_requests_response)
+    parameters = {
+        "principal": "EDI-3fa734a7cd6e40998a5c2b5486b6eced",
+        "eml": "<eml></eml>"
     }
-    private_key_path = "./data/private_key.pem"
-    algorithm = "ES256"
-    token = jwt.encode(payload, Path(private_key_path).read_text().encode("utf-8"), algorithm=algorithm)
-    return token
+    response = client.put(route="auth/v1/ping", parameters=parameters)
+    assert response.status_code == 200
+    assert response.body == '{"PUT": "OK"}'
 
+
+def test_delete(client, cookies, headers, mocker):
+    mock_requests_response = MagicMock(
+        status_code=200,
+        reason="OK",
+        headers=headers,
+        cookies=cookies,
+        text='{"DELETE": "OK"}'
+    )
+    mocker.patch.object(requests, "delete", return_value=mock_requests_response)
+    response = client.delete(route="auth/v1/ping")
+    assert response.status_code == 200
+    assert response.body == '{"DELETE": "OK"}'
