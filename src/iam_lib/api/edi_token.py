473f0aab6a31a118aa8096ebe59fdb3b1e394d23
Mark Servilla <mark.servilla@gmail.com>
2025-06-01 19:34:53 -0600

    Add EDI Token Client API

---
+++ src/iam_lib/api/edi_token.py
@@ -0,0 +1,103 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""
+:Mod:
+    edi_token
+
+:Synopsis:
+    IAM REST API edi_token client
+
+:Author:
+    servilla
+
+:Created:
+    6/1/25
+"""
+import daiquiri
+
+from iam_lib.api.client import Client
+import iam_lib.models.response_model as response_model
+
+
+logger = daiquiri.getLogger(__name__)
+
+
+class EdiTokenClient(Client):
+    """IAM EDI Token client class"""
+    
+    def __init__(
+            self,
+            scheme: str,
+            host: str,
+            accept: str,
+            public_key_path: str,
+            algorithm: str,
+            token: str,
+    ):
+        super().__init__(scheme, host, accept, public_key_path, algorithm, token)
+
+    def create_token(
+            self,
+            sub: str,
+    ) -> str:
+        """Create token.
+        Args:
+            sub (str): subject's unique EDI profile identifier
+
+        Returns:
+            edi_token (str): JWT base64 digitally signed token
+    
+        Raises:
+            iam_lib.exceptions.IAMRequestError: On HTTP request error
+            iam_lib.exceptions.IAMResponseError: On non-200 response
+    
+        """
+        route = "auth/v1/token"
+        form_params = {
+            "sub": sub,
+        }
+        self.post(route=route, form_params=form_params)
+        return response_model.response_data(self)
+    
+    
+    def revoke_token(
+            self,
+            sub: str
+    ) -> None:
+        """Revoke token.
+    
+        Args:
+            sub (str): subject's unique EDI profile identifier
+
+        Returns:
+            None
+    
+        Raises:
+            iam_lib.exceptions.IAMRequestError: On HTTP request error
+            iam_lib.exceptions.IAMResponseError: On non-200 response
+        """
+        route = f"auth/v1/token/{sub}"
+        self.put(route=route)
+        return None
+    
+    
+    def lock_token(
+            self,
+            sub: str
+    ) -> None:
+        """Lock tokens from being created.
+    
+        Args:
+            sub (str): subject's unique EDI profile identifier
+    
+        Returns:
+            None
+    
+        Raises:
+            iam_lib.exceptions.IAMRequestError: On HTTP request error
+            iam_lib.exceptions.IAMResponseError: On non-200 response
+        """
+        route = f"auth/v1/token/{sub}"
+        self.delete(route=route)
+        return None
---
+++ tests/test_edi_token_api.py
@@ -0,0 +1,73 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""
+:Mod:
+    test_edi_token_api
+
+:Synopsis:
+    Pytest for the EDI Token API
+
+:Author:
+    servilla
+
+:Created:
+    6/1/25
+"""
+from unittest.mock import MagicMock
+
+import daiquiri
+import requests
+
+from tests.fixtures import edi_token_client, cookies, headers
+
+
+logger = daiquiri.getLogger(__name__)
+
+
+def test_create_token(edi_token_client, cookies, headers, mocker):
+    mock_requests_response = MagicMock(
+        status_code=200,
+        reason="OK",
+        headers=headers,
+        cookies=cookies,
+        text="{'CREATE_TOKEN': 'OK'}"
+    )
+    mocker.patch.object(requests, "post", return_value=mock_requests_response)
+    edi_token_client.create_token(
+        sub="EDI-3fa734a7cd6e40998a5c2b5486b6eced",
+    )
+    assert edi_token_client.response.status_code == 200
+    assert edi_token_client.response.text == "{'CREATE_TOKEN': 'OK'}"
+
+
+def test_revoke_token(edi_token_client, cookies, headers, mocker):
+    mock_requests_response = MagicMock(
+        status_code=200,
+        reason="OK",
+        headers=headers,
+        cookies=cookies,
+        text="{'REVOKE_TOKEN': 'OK'}"
+    )
+    mocker.patch.object(requests, "put", return_value=mock_requests_response)
+    edi_token_client.revoke_token(
+        sub="EDI-3fa734a7cd6e40998a5c2b5486b6eced",
+    )
+    assert edi_token_client.response.status_code == 200
+    assert edi_token_client.response.text == "{'REVOKE_TOKEN': 'OK'}"
+
+
+def test_lock_token(edi_token_client, cookies, headers, mocker):
+    mock_requests_response = MagicMock(
+        status_code=200,
+        reason="OK",
+        headers=headers,
+        cookies=cookies,
+        text="{'LOCK_TOKEN': 'OK'}"
+    )
+    mocker.patch.object(requests, "delete", return_value=mock_requests_response)
+    edi_token_client.lock_token(
+        sub="EDI-3fa734a7cd6e40998a5c2b5486b6eced"
+    )
+    assert edi_token_client.response.status_code == 200
+    assert edi_token_client.response.text == "{'LOCK_TOKEN': 'OK'}"
